a17022bbb6a192d4d4c9d9b0bd372b4c575ce673
kms <kmu9842@naver.com>
2015-02-11 23:03:09 +0900

    revert onCreate()

--- QRmaze/proj.android/src/org/cocos2dx/cpp/AppActivity.java
+++ QRmaze/proj.android/src/org/cocos2dx/cpp/AppActivity.java
@@ -8,13 +8,5 @@ import android.os.Bundle;
 import com.google.zxing.integration.android.IntentIntegrator;
 
 public final class AppActivity extends Cocos2dxActivity {
-	
-	
-	@Override
-    protected void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); // Start
 
-        IntentIntegrator integrator = new IntentIntegrator(AppActivity.this);
-        integrator.initiateScan(IntentIntegrator.QR_CODE_TYPES);
-    }
 }
