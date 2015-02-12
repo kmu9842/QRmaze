e932492e6e31e4ed5a9a79927888e6d47b0fda75
kms <kmu9842@naver.com>
2015-02-12 23:58:16 +0900

    zxing reimport

--- QRmaze/proj.android/src/org/cocos2dx/cpp/AppActivity.java
+++ QRmaze/proj.android/src/org/cocos2dx/cpp/AppActivity.java
@@ -5,8 +5,6 @@ import org.cocos2dx.lib.Cocos2dxActivity;
 
 import android.os.Bundle;
 
-import com.google.zxing.integration.android.IntentIntegrator;
-
 public final class AppActivity extends Cocos2dxActivity {
 
 }
